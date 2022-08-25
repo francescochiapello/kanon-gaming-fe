1717744c62ecdc394c883affddd122d01dc7eb27
Francesco Chpl <francesco@chpl.it>
2022-08-25 16:58:56 +0200

    update prod url

--- src/Global/Config.js
+++ src/Global/Config.js
@@ -1,6 +1,6 @@
 const Config = {
-  // API_BASE_URL: 'https://kanonapi.chpl.it',
-  API_BASE_URL: 'http://localhost:5050',
+  API_BASE_URL: 'https://kanonapi.chpl.it',
+  // API_BASE_URL: 'http://localhost:5050',
   STORAGE_KEY: 'KANONGAMING',
   shadow: {
     WebkitBoxShadow: '0px 0px 5px 0px rgba(163,163,163,1)',
